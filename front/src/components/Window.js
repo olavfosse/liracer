9a80b548a4f21173ec6d73b1264ddf9f180c417b
Olav Fosse <fosseolav@gmail.com>
2020-09-06 15:29:44 +0200

    Fix Window overflowing downwards

--- front/src/components/Window.js
+++ front/src/components/Window.js
@@ -2,15 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 import colors from '../colors'
 
-const titleBarHeight = '30px';
+const titleBarHeight = '30px'
+const contentPadding = '1rem'
 
 const Titlebar = styled.div`
   height: ${titleBarHeight};
   background: ${colors.layer2Background};
 `
+
 const Content = styled.div`
-  height: calc(100% - ${titleBarHeight});
-  padding: 1rem;
+  height: calc(100% - ${titleBarHeight} - ${contentPadding} - ${contentPadding});
+  padding: ${contentPadding};
   background: ${colors.layer1Background};
 `
 
