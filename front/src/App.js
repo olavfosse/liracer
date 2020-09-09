0c4143b62fa97850c88c9cbf3249375cc8af9a16
Olav Fosse <fosseolav@gmail.com>
2020-09-09 19:58:33 +0200

    Close #1

--- front/src/App.js
+++ front/src/App.js
@@ -3,33 +3,38 @@ import styled from 'styled-components'
 import ChatAndButtons from './components/ChatAndButtons'
 import CodeField from './components/CodeField'
 import colors from './colors'
-
+import isMobile from 'ismobilejs'
 const Grid = styled.div`
-  // Consume the entire viewport
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  overflow: auto;
+// Consume the entire viewport
+position: fixed;
+top: 0;
+left: 0;
+bottom: 0;
+right: 0;
+overflow: auto;
 
-  // Use a grid layout
-  display: grid;
-  grid-template-columns: minmax(300px, 500px) minmax(750px, auto);
-  padding: 2rem;
-  column-gap: 2rem;
+// Use a grid layout
+display: grid;
+grid-template-columns: minmax(300px, 500px) minmax(750px, auto);
+padding: 2rem;
+column-gap: 2rem;
 
-  // Color
-  background: ${colors.layer0Background}
+// Color
+background: ${colors.layer0Background}
 `
 
 function App() {
-  return (
+  return isMobile(window.navigator).any ? (
+    <div>
+      <h3>This game is not playable on mobile devices</h3>
+      <p>To play liracer, open it on a laptop or desktop computer.</p>
+    </div>
+  ) : (
     <Grid>
       <ChatAndButtons />
       <CodeField />
     </Grid>
-  )
+  ) 
 }
 
 export default App
