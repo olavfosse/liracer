0800a4ab1c88e6656f996be85484240fbf27834c
Olav Fosse <fosseolav@gmail.com>
2020-09-09 21:07:43 +0200

    Layout chat and chat input in ChatAndButtons

--- front/src/components/ChatAndButtons.js
+++ front/src/components/ChatAndButtons.js
@@ -1,19 +1,45 @@
 import React from 'react'
 import Window from './Window'
 import styled from 'styled-components'
+import colors from '../colors'
 
 const contentPadding = '1rem'
+const inputHeight = '50px'
 
-const Wrapper = styled.div`
-  padding: ${contentPadding}
+const Messages = styled.div`
+  height: calc(100% - ${inputHeight} - ${contentPadding} - ${contentPadding});
+  padding: ${contentPadding};
+`
+
+const ChatForm = styled.form`
+  box-sizing: border-box;
+  width: auto;
+  height: 50px;
+  background: ${colors.layer2Background};
+  padding: calc(1em / 2);
+`
+
+const ChatInput = styled.input`
+  width: calc(100% - 1em);
+  height: 100%;
+  width: 100%;
+  border: 0;
+  margin: 0;
+  padding: 0;
+  background: ${colors.layer1Background}
 `
 
 const ChatAndButtons = () => {
   return (
     <Window>
-      <Wrapper>
-        Chat and buttons
-      </Wrapper>
+      <Messages>
+        <b>liracer</b> dit
+        <br/>
+        <b>liracer</b> dat
+      </Messages>
+      <ChatForm>
+        <ChatInput/>
+      </ChatForm>
     </Window>
   )
 }
