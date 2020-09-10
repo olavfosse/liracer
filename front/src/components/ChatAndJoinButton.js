4a19b2f647bfc56d041de309aa4459ecfd327584
Olav Fosse <fosseolav@gmail.com>
2020-09-10 21:40:37 +0200

    Close #2

--- front/src/components/ChatAndJoinButton.js
+++ front/src/components/ChatAndJoinButton.js
@@ -3,44 +3,65 @@ import Window from './Window'
 import styled from 'styled-components'
 import colors from '../colors'
 
-const contentPadding = '1rem'
-const inputHeight = '50px'
+const FlexBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
 
 const Messages = styled.div`
-  height: calc(100% - ${inputHeight} - ${contentPadding} - ${contentPadding});
-  padding: ${contentPadding};
+  flex-basis: 100%;
+  padding: 1rem;
 `
 
-const ChatForm = styled.form`
-  box-sizing: border-box;
-  width: auto;
+const ChatInputFormAndJoinButtonContainerSharedCSS = `
+  flex-basis: auto;
   height: 50px;
   background: ${colors.layer2Background};
-  padding: calc(1em / 2);
+  padding: calc(1rem / 2);
+  box-sizing: border-box;
 `
 
-const ChatInput = styled.input`
-  width: calc(100% - 1em);
+const ChatInputForm = styled.form`${ChatInputFormAndJoinButtonContainerSharedCSS}`
+
+const JoinButtonContainer = styled.div`${ChatInputFormAndJoinButtonContainerSharedCSS}`
+
+const JoinButtonAndChatInputSharedCSS = `
+  border: 0;
   height: 100%;
   width: 100%;
-  border: 0;
-  margin: 0;
   padding: 0;
-  background: ${colors.layer1Background}
+  background: ${colors.layer1Background};
+  outline: none; // CONSIDER removing this line. The outline makes it clear what box is selected although it does look ugly. If it is enabled again it should also be enabled on CodeField.
+`
+
+const ChatInput = styled.input`${JoinButtonAndChatInputSharedCSS}`
+
+const JoinButton = styled.button`
+  ${JoinButtonAndChatInputSharedCSS}
+  transition-duration: 0.1s;
+  font-size: 17px;
+  :hover {
+    background: ${colors.layer2Background};;
+  }
 `
 
 const ChatAndJoinButton = () => {
   return (
     <Window>
-      <Messages>
-        <b>liracer</b> dit
-        <br/>
-        <b>liracer</b> dat
-      </Messages>
-      <ChatForm>
-        <ChatInput/>
-      </ChatForm>
-    {/* The chat button is going here, hence the name */}
+      <FlexBox>
+        <Messages>
+          <b>liracer</b> dit
+          <br/>
+          <b>liracer</b> dat
+        </Messages>
+        <ChatInputForm>
+          <ChatInput/>
+        </ChatInputForm>
+        <JoinButtonContainer>
+          <JoinButton onClick={() => alert('This will prompt a game id and join it. Alternatively it could turnn into a text field when clicked where the game id can be entered.')}>JOIN GAME</JoinButton>
+        </JoinButtonContainer>
+      </FlexBox>
     </Window>
   )
 }
