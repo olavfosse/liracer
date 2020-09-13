391df4f6e6af1895aaa0fbb195823a514951c4ec
Olav Fosse <fosseolav@gmail.com>
2020-09-14 00:38:11 +0200

    Redo the chat input and join button section

--- front/src/components/ChatAndJoinButton.js
+++ front/src/components/ChatAndJoinButton.js
@@ -1,3 +1,36 @@
+// const InputContainer = styled.div`
+//   background: ${colors.layer2Background};
+//   flex-basis: auto;
+//   display: flex;
+//   height: 50px;
+//   padding: calc(1rem / 2);
+//   box-sizing: border-box;
+// `
+
+// const JoinButtonContainer = styled.div`
+//   background: ${colors.layer2Background};
+//   flex-basis: 90px;
+//   padding-left: 1em;
+// `
+
+// const JoinButton = styled.button`
+//   :hover {
+  //     background: ${colors.layer2Background};
+  //     color: ${colors.layer2Foreground};
+  //   }
+  //   background: ${colors.layer1Background};
+  //   color: ${colors.layer1Foreground};
+  //   transition-duration: 0.12s;
+  //   height: 100%;
+  //   width: 100%;
+  //   font-weight: bold;
+  //   font-size: 14px;
+  //   padding: 0;
+//   border: solid ${colors.layer2Background} 0;
+//   border-radius: 5px;
+//   cursor: pointer;
+//   outline: none;
+// `
 import React from 'react'
 import Window from './Window'
 import styled from 'styled-components'
@@ -12,38 +45,63 @@ const FlexBox = styled.div`
 const Messages = styled.div`
   flex-basis: 100%;
   padding: 1rem;
+  color: ${colors.layer1Foreground};
 `
 
-const ChatInputFormAndJoinButtonContainerSharedCSS = `
-  flex-basis: auto;
-  height: 50px;
+const ChatForm = styled.form`
   background: ${colors.layer2Background};
+  height: 50px;
+  display: flex;
   padding: calc(1rem / 2);
   box-sizing: border-box;
 `
 
-const ChatInputForm = styled.form`${ChatInputFormAndJoinButtonContainerSharedCSS}`
-
-const JoinButtonContainer = styled.div`${ChatInputFormAndJoinButtonContainerSharedCSS}`
-
-const JoinButtonAndChatInputSharedCSS = `
-  border: 0;
-  height: 100%;
-  width: 100%;
-  padding: 0;
+const ChatInput = styled.input`
   background: ${colors.layer1Background};
-  outline: none; // CONSIDER removing this line. The outline makes it clear what box is selected although it does look ugly. If it is enabled again it should also be enabled on CodeField.
+  color: ${colors.layer1Foreground};
+  border: 0;
+  width: 100%;
+  padding: 0 3px 3px 0;
+  box-sizing: border-box;
+  outline: none;
+  border-right: 1em solid ${colors.layer2Background};
 `
 
-const ChatInput = styled.input`${JoinButtonAndChatInputSharedCSS}`
+const buttonCSS = `
+  :hover {
+    background: ${colors.layer2Background};
+    color: ${colors.layer2Foreground};
+  }
+  background: ${colors.layer1Background};
+  color: ${colors.layer1Foreground};
+  transition-duration: 0.12s;
+  font-weight: bold;
+  font-size: 14px;
+  padding: 0;
+  cursor: pointer;
+  border: 0;
+  border-radius: 5px;
+  outline: none;
+`
+
+const ChatSubmit = styled.button`
+  ${buttonCSS}
+  width: 70px;
+  border: solid ${colors.layer2Background} 0;
+`
+
+const JoinButtonContainer = styled.div`
+  border: calc(1em / 2) solid ${colors.layer2Background};
+  border-top: 0;
+  height: 40px;
+  box-sizing: border-box;
+  background: ${colors.layer2Background};
+`
 
 const JoinButton = styled.button`
-  ${JoinButtonAndChatInputSharedCSS}
-  transition-duration: 0.1s;
-  font-size: 1em;
-  :hover {
-    background: ${colors.layer2Background};;
-  }
+  ${buttonCSS}
+  height: 100%;
+  width: 100%;
 `
 
 const ChatAndJoinButton = () => {
@@ -51,15 +109,17 @@ const ChatAndJoinButton = () => {
     <Window>
       <FlexBox>
         <Messages>
-          <b>liracer</b> dit
+          <b>&lt;liracer&gt;</b> Click the JOIN button or type /join &lt;gameid&gt; to join a game. 
           <br/>
-          <b>liracer</b> dat
         </Messages>
-        <ChatInputForm>
-          <ChatInput/>
-        </ChatInputForm>
+        <ChatForm onSubmit={(event) => event.preventDefault() }>
+          <ChatInput />
+          <ChatSubmit>SEND</ChatSubmit>
+        </ChatForm>
         <JoinButtonContainer>
-          <JoinButton onClick={() => alert('This will prompt a game id and join it. Alternatively it could turnn into a text field when clicked where the game id can be entered.')}>JOIN GAME</JoinButton>
+          <JoinButton onClick={() => prompt('GameID')}>
+            JOIN GAME
+          </JoinButton>
         </JoinButtonContainer>
       </FlexBox>
     </Window>
