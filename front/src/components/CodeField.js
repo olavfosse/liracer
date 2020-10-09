fa362914f5ab9e60e49401dd9bf9980d59b6cb6e
Olav Fosse <fosseolav@gmail.com>
2020-10-09 13:01:53 +0200

    Make CodeField color layer1Foreground

--- front/src/components/CodeField.js
+++ front/src/components/CodeField.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Window from './Window'
 import styled from 'styled-components'
+import colors from '../colors'
 
 const contentPadding = '1rem'
 
 const Wrapper = styled.pre`
   padding: ${contentPadding};
   margin: 0;
+  color: ${colors.layer1Foreground}
 `
 
 const CodeField = () => {
