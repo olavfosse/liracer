9370d545dfbe506be610ac79fea9cf6e6ca2eefa
Olav Fosse <fosseolav@gmail.com>
2020-10-10 22:33:01 +0200

    Fix indentation in hello_world.java

--- models/quotes/java/hello_world.java
+++ models/quotes/java/hello_world.java
@@ -1,5 +1,5 @@
 class helloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
 }
