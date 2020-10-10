102e554ee4e73d24a5c9cec18b49dc0d59a90192
GMCgit <goranivancic117@gmail.com>
2020-10-10 22:58:09 +0200

    tab size fix

--- models/quotes/javascript/bubbleSort.js
+++ models/quotes/javascript/bubbleSort.js
@@ -1,13 +1,12 @@
 function bubbleSort(array) {
-  for (i = 0; i < array.length-1; i++) {
-    for (j = 0; j < array.length-1; j++) {
-      if (array[j] > array[j+1]) {
-        let temp = array[j+1];
-        array[j+1] = array[j]
-        array[j] = temp;
-      }
-    }
-  }
-
-  return array;
+	for (i = 0; i < array.length - 1; i++) {
+		for (j = 0; j < array.length - 1; j++) {
+    		if (array[j] > array[j + 1]) {
+				let temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+	return array;
 }
