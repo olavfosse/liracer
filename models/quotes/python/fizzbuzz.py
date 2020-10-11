ad1048db5fb71edbf16382d24318fe2434f06c21
GMCgit <goranivancic117@gmail.com>
2020-10-11 18:05:45 +0200

    replaced space with tab

--- models/quotes/python/fizzbuzz.py
+++ models/quotes/python/fizzbuzz.py
@@ -1,12 +1,11 @@
 def fizzbuzz(n):
-    for i in range(0, n+1, 1):
-        message = ""
-        if i % 3 == 0:
-            message += "Fizz"
-        if i % 5 == 0:
-            message+= "Buzz"
+	for i in range(0, n+1, 1):
+		message = ""
+		if i % 3 == 0:
+			message += "Fizz"
+		if i % 5 == 0:
+			message+= "Buzz"
+		if message == "":
+			message = i
 
-        if message == "":
-            message = i
-
-        print(message) 
+		print(message) 
