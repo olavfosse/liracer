f75fc0d025a30a27f3a6ce791b45925fbba855fc
hungthezorba <hungthezorba@gmail.com>
2020-10-14 14:14:26 +0700

    added CSS to apply scrollbar when messages overflow

--- front/src/components/Messages.js
+++ front/src/components/Messages.js
@@ -19,6 +19,10 @@ const Message = (props) => {
 const MessagesDiv = styled.div`
   flex-basis: 100%;
   padding: 1rem;
+  // Scrollbar for message box
+  flex: 1 1 auto; 
+  overflow-y: auto;
+  height: 0px;
   color: ${colors.layer1Foreground};
   // Copied from default React index.css
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
