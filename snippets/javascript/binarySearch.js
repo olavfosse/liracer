c956f26751927bc0267edf8a8c3a53ec824fb1b1
GMCgit <goranivancic117@gmail.com>
2020-10-14 12:03:35 +0200

    fixed tabs

--- snippets/javascript/binarySearch.js
+++ snippets/javascript/binarySearch.js
@@ -1,21 +1,21 @@
 let doSearch = function (array, targetValue) {
-  let min = 0;
-  let max = array.length - 1;
-  let guess;
-  let NOfGuesses = 0;
+	let min = 0;
+	let max = array.length - 1;
+	let guess;
+	let NOfGuesses = 0;	
 
-  while (max >= min) {
-    guess = Math.floor((min + max) / 2);
-    NOfGuesses++;
-    if (array[guess] === targetValue) {
-      println(NOfGuesses);
-      return guess;
-    } else if (array[guess] < targetValue) {
-      min = guess + 1;
-    } else {
-      max = guess - 1;
-    }
-  }
+	while (max >= min) {
+		guess = Math.floor((min + max) / 2);
+		NOfGuesses++;
+		if (array[guess] === targetValue) {
+			println(NOfGuesses);
+			return guess;
+		} else if (array[guess] < targetValue) {
+			min = guess + 1;
+		} else {
+			max = guess - 1;
+		}
+	}	
 
-  return -1;
+	return -1;
 };
