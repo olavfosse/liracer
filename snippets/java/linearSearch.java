4e08b1ad98d724f4a39878a07677dc20faffc38a
GMCgit <69117291+GMCgit@users.noreply.github.com>
2020-10-14 21:27:49 +0200

    Update linearSearch.java

--- snippets/java/linearSearch.java
+++ snippets/java/linearSearch.java
@@ -1,7 +1,7 @@
 public static int linear_search(int[] array, int target) {
 	for (int i = 0; i < array.length; i++) {
 		if (array[i] == target) {
-			return i+1;
+			return i;
 		}
 	}
 	return -1;
