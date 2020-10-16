29d666352aa2dc23da08530db74f23062d5a19c6
hungthezorba <hungthezorba@gmail.com>
2020-10-16 07:55:46 +0700

    Changes from using className to styled-components

--- front/src/components/Messages.js
+++ front/src/components/Messages.js
@@ -3,18 +3,40 @@ import styled from 'styled-components'
 import colors from '../colors'
 import './ToolTip.css'
 
+const ToolTipText = styled.span`
+  visibility: hidden;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0px;
+  left:0;
+  top:20px;
+  opacity: 0.8;
+  
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+`
+
 const MessageDiv = styled.div`
   // Make it easy to visually differentiate messages
-  padding-bottom: 7px;
+  &{
+    padding-bottom: 7px;
+    position: relative;
+  }
+  &:hover ${ToolTipText} {
+    visibility: visible;
+  }
 `
 
 const Message = (props) => {
   console.log(props.message.playerID)
   return (
-    <MessageDiv className="tooltip">
+    <MessageDiv>
       {/* IMPORTANT SPACE --> */}
       <b>&lt;{props.message.sender}&gt;</b> {props.message.content}
-      <span className={props.message.playerID ? 'tooltiptext' : ''}>{props.message.playerID}</span>
+      {props.message.playerID ? <ToolTipText>{props.message.playerID}</ToolTipText> : ''}
     </MessageDiv>
   )
 }
@@ -31,11 +53,12 @@ const MessagesDiv = styled.div`
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
 `
 
+
 const Messages = (props) => {
   return (
     <MessagesDiv>
       {
-        props.messages.map((message, index) => <Message key={index} message={ message }/>)
+        props.messages.map((message, index) => <Message key={index} message={message} />)
       }
     </MessagesDiv>
   )
