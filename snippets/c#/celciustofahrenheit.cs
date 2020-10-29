8fc94814a0a1965fdbfbea40094f0bf5cd18c968
SamudraThio <68200757+SamudraThio@users.noreply.github.com>
2020-10-28 19:33:55 -0500

    Added C# files.
    - fizzbuzz.cs
    - fahrenheittocelcius.cs
    - celciustofahrenheit.cs

---
+++ snippets/c#/celciustofahrenheit.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static double CelciusToFahrenheit(double celcius)
+    {
+        double fahrenheit = (celcius * 9 / 5) + 32;
+        return fahrenheit;
+    }
+}
---
+++ snippets/c#/fahrenheittocelcius.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static double FahrenheitToCelcius(double fahrenheit)
+    {
+        double celcius = (fahrenheit - 32) * 5 / 9;
+        return celcius;
+    }
+}
+
