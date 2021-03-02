f3dbf3fc58c191453935fc1c2980011a05accdc9
Olav Fosse <mail@olavfosse.no>
2021-03-02 10:04:36 +0100

    Add helper functions for HandlerFunc

--- gamehubplayer/player/player.go
+++ gamehubplayer/player/player.go
@@ -9,41 +9,8 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type PlayerID int
-
-type Player struct {
-	// ID identifies Player.
-	ID PlayerID
-	// Send is a channel for writing to Player's underlying connection.
-	Send chan []byte
-}
-
-// playerIdEmitter generates PlayerIds. It first produces PlayerId(1). Thereafter
-// it always produces PlayerIds one greater than the previous, I.e PlayerId(1),
-// PlayerId(2), ..., PlayerId(n).
-var playerIdEmitter = make(chan PlayerID)
-
-func init() {
-	// No cleanup is necessary
-	go func() {
-		nextID := PlayerID(1)
-		for {
-			playerIdEmitter <- nextID
-			nextID++
-		}
-	}()
-}
-
-// New creates a Player with a unique id. It initializes send, but does not
-// start reading from it. That job is reserved for the caller.
-func New() *Player {
-	return &Player{
-		ID:   <-playerIdEmitter,
-		Send: make(chan []byte),
-	}
-}
-
-// HandlerFunc is used with http.HandleFunc to set up a websocket endpoint.
+// HandlerFunc is a http handler function, for use with http.HandleFunc, which
+// is used to set up a WebSocket endpoint that players interact with.
 func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	upgrader := websocket.Upgrader{
 		ReadBufferSize:  1024,
@@ -55,38 +22,46 @@ func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
-	p := New()
+	p := &Player{
+		ID:   <-playerIdEmitter,
+		conn: conn, // MUST NOT BE WRITTEN DIRECTLY TO.
+		Send: make(chan []byte),
+	}
+	go p.pumpFromSend()
+	p.handleIncomingMessages()
+}
 
-	// To prevent reading/writing concurrently to conn we discard it and
-	// discard of the read/write methods outside the goroutine where they
-	// should be used.
-	writeMessage := conn.WriteMessage
-	readMessage := conn.ReadMessage
-	conn = nil
-	go func() {
-		wm := writeMessage
-		writeMessage = nil // we may only write from within this goroutine
-
-		for {
-			bs := <-p.Send
-			err := wm(websocket.TextMessage, bs)
-			log.Printf("wrote: %q\n", bs)
-			if err != nil {
-				// TODO: actually close it
-				log.Println("error(closing connection):", err)
-				return
-			}
-		}
-	}()
-
+// pumpFromSend continually pumps the messages from p.Send to p's underlying
+// connection.
+//
+// THIS IS THE ONLY FUNCTION PERMITTED TO WRITE DIRECTLY TO p's UNDERLYING
+// CONNECTION. ONLY ONE pumpFromSend GOROUTINE IS PERMITTED TO EXIST PER p.
+func (p *Player) pumpFromSend() {
 	for {
-		_, bs, err := readMessage()
+		bs := <-p.Send
+		err := p.conn.WriteMessage(websocket.TextMessage, bs)
 		if err != nil {
 			// TODO: actually close it
 			log.Println("error(closing connection):", err)
 			return
 		}
-		log.Printf("read: %q\n", bs)
+	}
+}
+
+// handleIncomingMessages continually reads and handles incoming messages from
+// p's underlying connection.
+//
+// THIS IS THE ONLY FUNCTION PERMITTED TO READ DIRECTLY FROM p's UNDERLYING
+// CONNECTION. ONLY ONE handleIncomingMessages GOROUTINE IS PERMITTED TO EXIST
+// PER p.
+func (p *Player) handleIncomingMessages() {
+	for {
+		_, bs, err := p.conn.ReadMessage()
+		if err != nil {
+			// TODO: actually close it
+			log.Println("error(closing connection):", err)
+			return
+		}
 		var m incomingMsg
 		err = json.Unmarshal(bs, &m)
 		if err != nil {
@@ -113,5 +88,33 @@ func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 		if !isMessageHandled {
 			log.Printf("error: unhandled message: %q\n", bs)
 		}
+
 	}
+}
+
+type PlayerID int
+
+type Player struct {
+	// ID identifies Player.
+	ID PlayerID
+	// conn is Player's underlying connection.
+	conn *websocket.Conn
+	// Send is a channel for writing to Player's underlying connection.
+	Send chan []byte
+}
+
+// playerIdEmitter generates PlayerIds. It first produces PlayerId(1). Thereafter
+// it always produces PlayerIds one greater than the previous, I.e PlayerId(1),
+// PlayerId(2), ..., PlayerId(n).
+var playerIdEmitter = make(chan PlayerID)
+
+func init() {
+	// No cleanup is necessary
+	go func() {
+		nextID := PlayerID(1)
+		for {
+			playerIdEmitter <- nextID
+			nextID++
+		}
+	}()
 }
